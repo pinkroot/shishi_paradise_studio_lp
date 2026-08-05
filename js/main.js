ed85fb5a6ddc4bf37851b3cc2c8bfd2d01afe704
Shun-Ichiro Kaneshiro <pinkroot@gmail.com>
2026-08-05 18:05:11 +0900

    038: MV公開 yt=HjRIR3B7nHs

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ const SONGS = [
   { no: "035", title: "Nachos! Just One More!", yt: "1XSCQKRg1aA" },
   { no: "036", title: "Yo! Yogurt", yt: "2a28CGXu0yU" },
   { no: "037", title: "Croissant Spiral", yt: "Odf1nhjqP-I" },
-  { no: "038", title: "Biri Biri Biryani", yt: null },
+  { no: "038", title: "Biri Biri Biryani", yt: "HjRIR3B7nHs" },
   { no: "039", title: "Hole Is Bagel Soul", yt: "_2DVs6AA6PM" },
 ];
 
